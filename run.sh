95e9b510c972b9c26ada161bda38340a47e47075
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2016-01-31 13:50:08 +0100

    reorder run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+if [ "`curl http://127.0.0.1:8067/up 2> /dev/null`" = "up" ]
+then
+    exit
+fi
+
 cd `dirname $0`
 
-if [ "`curl http://127.0.0.1:8067/up 2> /dev/null`" != "up" ]
+ln -s lock.$$ lock
+if [ "`readlink lock`" != lock.$$ ]
 then
-    killall pqserve
-    sleep 2
-    killall -KILL pqserve
-    sleep 2
-    echo Restarting pqserve
-    cat pqserve.out
-    bin/pqserve &> pqserve.out &
+    echo Getting lock failed
+    exit
 fi
+
+killall pqserve
+sleep 2
+killall -KILL pqserve
+sleep 2
+echo Restarting pqserve
+cat pqserve.out
+bin/pqserve &> pqserve.out &
+
+rm -f lock
